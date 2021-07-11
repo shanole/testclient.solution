4e6a1da3874fc59ff9b210a5600d5317c18decfe
shanole <73411129+shanole@users.noreply.github.com>
2021-07-11 12:11:09 -0700

    successful test for binding base64 data to model

--- TestClient/Controllers/ImagesController.cs
+++ TestClient/Controllers/ImagesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using TestClient.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace ScribbleBoard.Controllers
 {
@@ -13,14 +14,20 @@ namespace ScribbleBoard.Controllers
             var allImages = Image.GetAll();
             return View(allImages);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
         [HttpPost]
         public IActionResult Create(Image image)
         {
             // var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             // var currentUser = await _userManager.FindByIdAsync(userId);
             // image.User = currentUser;
+            // image.UserId = userId;
+            Console.WriteLine("image Data is" + image.Description);
             Image.Post(image);
-            return RedirectToAction("Index");
+            return RedirectToAction("Create");
         }
         public IActionResult Details(int id)
         {
